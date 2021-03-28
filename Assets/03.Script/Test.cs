517df6d30c66c4fe81c0d4d797e76b950d43044a
DESKTOP-OHD6DA9\USER <sue9340@gmail.com>
2021-03-29 00:46:11 +0900

    랜덤 테스트

---
+++ Assets/03.Script/Test.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    int a;
+    public void teat()
+    {
+        a = Random.Range(1, 20); //1부터 19까지 출력
+        Debug.Log(a);
+    }
+}
