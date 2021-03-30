079236103e40fd3b98930201d9c87ae2888edf60
DESKTOP-OHD6DA9\USER <sue9340@gmail.com>
2021-03-31 00:33:39 +0900

    랜덤 시간마다 출력되고 기다린 시간도 출력됨

--- Assets/03.Script/Test.cs
+++ Assets/03.Script/Test.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    /*
+     1   2   3
+     4   5   6
+     7   8   9
+     
+     */
+    private void Start()
+    {
+        StartCoroutine("TestTime");
+    }
     int a;
+    IEnumerator TestTime()
+    {
+        while(true) //매 프레임마다 실행
+        {
+            a = Random.Range(1, 20); //1부터 19까지 출력
+
+            //3초동안 랜덤숫자 실행됨
+            yield return new WaitForSeconds(a); //3초 기다림
+            Debug.Log(a);
+        }
+    }
     public void teat()
     {
-        a = Random.Range(1, 20); //1부터 19까지 출력
-        Debug.Log(a);
+        
     }
 }
