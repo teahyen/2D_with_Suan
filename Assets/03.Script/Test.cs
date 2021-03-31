ad3596b5750d805fc156aaaa45c188e52e85c9f1
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-03-31 20:35:47 +0900

    일단 작업

--- Assets/03.Script/Test.cs
+++ Assets/03.Script/Test.cs
@@ -8,7 +8,6 @@ public class Test : MonoBehaviour
      1   2   3
      4   5   6
      7   8   9
-     
      */
     private void Start()
     {
---
+++ Assets/03.Script/spawnSuan.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnSuan : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Suan;
+
+    private int RandomPos;
+    private int RandomTime;
+
+    void Start()
+    {
+        StartCoroutine("spawn");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator spawn()
+    {
+        RandomTime = Random.Range(1, 5);
+        RandomPos = (int)Random.Range(1,10);     //(int) 는 float, long등 을 int 값으로 바꿔준다. 반대로도 가능;
+        yield return new WaitForSeconds(RandomTime);
+        if(RandomPos == 1)
+            Instantiate(Suan, new Vector3(-6,3,0), transform.rotation);
+        else if (RandomPos == 2)
+            Instantiate(Suan, new Vector3(0, 3, 0), transform.rotation);
+        else if (RandomPos == 3)
+            Instantiate(Suan, new Vector3(6, 3, 0), transform.rotation);
+        else if (RandomPos == 4)
+            Instantiate(Suan, new Vector3(-6, 0, 0), transform.rotation);
+        else if (RandomPos == 5)
+            Instantiate(Suan, new Vector3(0, 0, 0), transform.rotation);
+        else if (RandomPos == 6)
+            Instantiate(Suan, new Vector3(6, 0, 0), transform.rotation);
+        else if (RandomPos == 7)
+            Instantiate(Suan, new Vector3(-6, -3, 0), transform.rotation);
+        else if (RandomPos == 8)
+            Instantiate(Suan, new Vector3(0, -3, 0), transform.rotation);
+        else if (RandomPos == 9)
+            Instantiate(Suan, new Vector3(6, -3, 0), transform.rotation);
+
+    }
+}
