62d56d849f9ce3281c2636d22786559d29805ecc
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-04-03 21:41:08 +0900

    Revert "풀메니져"
    This reverts commit f2179c97126ff66589c5d1c3cb125581325e6ee8.

--- Assets/03.Script/GameManager.cs
+++ Assets/03.Script/GameManager.cs
@@ -4,27 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    public static GameManager Instance
+    // Start is called before the first frame update
+    void Start()
     {
-        get
-        {
-            if (instance == null)
-            {
-                instance = FindObjectOfType<GameManager>();
-
-                if (instance == null)
-                {
-                    GameObject temp = new GameObject("GameManager");
-                    instance = temp.AddComponent<GameManager>();
-                }
-            }
-
-            return instance;
-        }
+        
     }
-    public PoolManager pool;
-    public int score = 0;
+
     // Update is called once per frame
     void Update()
     {
